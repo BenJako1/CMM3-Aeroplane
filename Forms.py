544a203ae34ef7214c222d56a36c7fc3ad595b15
BenJako1 <132501374+BenJako1@users.noreply.github.com>
2023-10-25 17:35:49 +0100

    cleaned up
    moved a lot to the other files

--- Forms.py
+++ Forms.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 23 16:54:04 2023
+'''
 
-@author: mauri
-"""
+CMM3 Group 7
+Benjamin, Rodrigo, Maurice, Nick, Jack, Stamatis
+October-November 2023  
+
+'''
 
 #This module, 'Forms', is used to define the formulas of the aircraft dynamics without having to include them 
 #in the main script. This module imports variables from the 'constants'module so that it recognizes variables such as 
 #CL0, CLa, and so on. 
 import numpy as np
+from scipy.optimize import newton
 import constants
 
 def Coefficient_of_Lift(a, d):
@@ -20,21 +23,20 @@ def Coefficient_of_Moment(a, d):
 def Coefficient_of_Drag(a, d):
     return constants.CD0 + constants.K * (Coefficient_of_Lift(a, d))**2
 
-def Lift(a, d, velocity, gamma):
+def Lift(a, d, velocity):
     return (0.5 * constants.air_density * velocity**2 * constants.wing_surface *
             Coefficient_of_Lift(a, d))
 
-def Drag(a, d, velocity, gamma):
+def Drag(a, d, velocity):
     return (0.5 * constants.air_density * velocity**2 * constants.wing_surface *
             Coefficient_of_Drag(a, d))
 
-def Moment(a, d, velocity, gamma):
+def Moment(a, d, velocity):
     return (0.5 * constants.air_density * velocity**2 * constants.wing_surface *
            constants.cbar * Coefficient_of_Moment(a, d))
 
-def Engine_Thrust(a, d, the, velocity, gamma):
-    return (0.5 * constants.air_density * velocity**2 * constants.wing_surface *
-            Coefficient_of_Drag(a, d) * np.cos(a) +
+def Engine_Thrust(a, d, the, velocity):
+    return (Drag(a, d, velocity) * np.cos(a) +
             constants.mass * constants.gravity * np.sin(the) -
             0.5 * constants.air_density * velocity**2 * constants.wing_surface *
             Coefficient_of_Lift(a, d) * np.sin(a))
@@ -45,3 +47,23 @@ def Equilibrium(a, velocity, gamma):
             0.5 * constants.air_density * velocity**2 * constants.wing_surface *
             (constants.CD0 + constants.K * (constants.CL0 + constants.CLa * a - constants.CLde * (constants.CM0 + constants.CMa * a) / constants.CMde)**2) * np.sin(a) +
             constants.mass * constants.gravity * np.cos(a + gamma))
+
+def Trim(velocity, gamma):
+    # Define the equilibrium equation as f(a)
+    def f(a):
+        return Equilibrium(a, velocity, gamma)   
+
+    # Solve for alpha and delta
+    initial_guess = 0.01  # Provide an initial guess
+    alpha = newton(f, initial_guess)
+    delta = -(constants.CM0 + constants.CMa * alpha)/constants.CMde
+
+    # Calculating other variables to output
+    theta = alpha + gamma
+    ub = velocity * np.cos(alpha)
+    wb = velocity * np.sin(alpha)
+
+    # Calculating Thrust
+    thrust = Drag(alpha, delta, velocity) * np.cos(alpha) - Lift(alpha, delta, velocity) * np.sin(alpha) + constants.mass * constants.gravity * np.sin(theta)
+    
+    return alpha, delta, theta, ub, wb, thrust
--- simulation.py
+++ simulation.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+
+CMM3 Group 7
+Benjamin, Rodrigo, Maurice, Nick, Jack, Stamatis
+October-November 2023  
+
+'''
 
 # importing modules
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import newton
-import constants
-import forms
+import constants, forms
 
 #-----------------------------------------------------------------------------------------------------------
 # User parameters
@@ -13,57 +19,10 @@ velocity = 100  # Aircraft velocity in m/s
 gamma = 0.00   # Path angle in rad
 
 #-----------------------------------------------------------------------------------------------------------
-# Defining functions for later use. The functions are derived from the project breif.
+# Trim & output
 
-def CL (a, d):
-    return forms.Coefficient_of_Lift(a, d)
+alpha, delta, theta, ub, wb, thrust = forms.Trim(velocity, gamma)
 
-def CM (a, d):
-    return forms.Coefficient_of_Moment(a, d)
-
-def CD (a, d):
-    return forms.Coefficient_of_Drag(a, d)
-
-def L (a, d):
-    return forms.Lift(a, d, velocity, gamma)
-
-def D (a, d):
-    return forms.Drag(a, d, velocity, gamma)
-
-def M (a, d):
-    return forms.Moment(a, d, velocity, gamma)
-
-def Thrust (a, d, the):
-    return forms.Engine_Thrust(a, d, the, velocity, gamma)
-
-# Runge-Kutta method for integral solving. Parameters from dx/dt = f
-def RK4(x, f, dt):
-    k1 = f
-    k2 = f + 0.5 * dt * k1
-    k3 = f + 0.5 * dt * k2
-    k4 = f + dt * k3
-    
-    return x + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-
-#-----------------------------------------------------------------------------------------------------------
-# Final calculations & output
-
-# Define the equilibrium equation as f(a)
-def f(a):
-    return forms.Equilibrium(a, velocity, gamma)   
-
-# Solve for alpha and delta
-initial_guess = 0.01  # Provide an initial guess
-alpha = newton(f, initial_guess)
-delta = -(constants.CM0 + constants.CMa * alpha)/constants.CMde
-
-# Calculating other variables to output
-theta = alpha + gamma
-ub = velocity * np.cos(alpha)
-wb = velocity * np.sin(alpha)
-
-# Calculating Thrust
-thrust = Thrust(alpha, delta, theta) 
 
 print(f"alpha = {alpha}")
 print(f"delta = {delta}")
@@ -71,8 +30,6 @@ print(f"thrust = {thrust}")
 print(f"theta = {theta}")
 print(f"ub = {ub}")
 print(f"wb = {wb}")
-#Moment replaced with M
-print(M(alpha, delta))
 
 #-----------------------------------------------------------------------------------------------------------
 # Applying Euler method for solving differential DOF equations
@@ -86,7 +43,7 @@ xe = 0
 ze = 0
 t = t0
 
-moment = 0
+moment = forms.Moment(alpha, delta, velocity)
 
 tValues = [t0]
 thetaValues = [theta]
@@ -102,23 +59,24 @@ momentValues = [moment]
 while t < tEnd:
     if t >= 1:
         delta = -0.0572
+    
     # Compute new values using the DOF equations
-    theta = RK4(theta, q, dt)
+    theta += q * dt
     alpha = np.arctan2(wb, ub)
     gamma = theta - alpha
-    #Moment replaced with M
-    moment = M(alpha, delta)
-    thrust = Thrust(alpha, delta, theta)
-    q = RK4(q, (moment/constants.inertia_yy), dt)
+    moment = forms.Moment(alpha, delta, velocity)
+    thrust = forms.Engine_Thrust(alpha, delta, theta, velocity)
+    q += (moment/constants.inertia_yy) * dt
     xe += (ub * np.cos(theta) + wb * np.sin(theta)) * dt
     ze -= (- ub * np.sin(theta) + wb * np.cos(theta)) * dt
-    ub += (L(alpha, delta) * np.sin(alpha) / constants.mass - D(alpha, delta) *
+    ub += (forms.Lift(alpha, delta, velocity) * np.sin(alpha) / constants.mass - forms.Drag(alpha, delta, velocity) *
            np.cos(alpha) / constants.mass - q * wb - constants.gravity * np.sin(theta) +
            thrust/constants.mass) * dt
-    wb += (- L(alpha, delta) * np.cos(alpha) / constants.mass - D(alpha, delta) *
+    wb += (- np.cos(alpha) * forms.Lift(alpha, delta, velocity) / constants.mass - forms.Drag(alpha, delta, velocity) *
            np.sin(alpha) / constants.mass + q * ub + constants.gravity * np.cos(theta)) * dt
     # Append new values to arrays
     t += dt
+    
     tValues.append(round(t, 1))
     thetaValues.append(theta)
     qValues.append(q)
