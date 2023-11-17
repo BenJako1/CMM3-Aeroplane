80ee82942ebe8a42956790b0236d94dc10b6befb
BenJako1 <132501374+BenJako1@users.noreply.github.com>
2023-11-17 13:04:56 +0000

    Revert "Merge branch 'A3_B1_B2-running-and-annotated' of https://github.com/BenJako1/CMM3-Aeroplane into A3_B1_B2-running-and-annotated"
    This reverts commit 404e0cf8c9a4f9bd0b9fd591498c611e6d50bae1, reversing
    changes made to 11e410f978afb2e4c1da8c4453754c13da5618a9.

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 CMM3 Group 7
 Benjamin, Rodrigo, Maurice, Nick, Jack, Stamatis
@@ -305,6 +306,5 @@ if __name__ == "__main__":
     print(f"Angle of Attach: {np.rad2deg(trim.alpha): 0.3f} Degrees")
     print(f"Required Thrust: {trim.thrust: 0.3f} N")
     '''
-
    # f"Climb Duration: {self.climbTime}s
 
